dd0d5c6a1a09e0597eda6dddf47030c6b39bfada
Dhruv Aggarwal <adhruv218@gmail.com>
2024-04-03 18:15:39 +0530

    api intrgration bug fixed

--- client/src/pages/Singup/index.jsx
+++ client/src/pages/Singup/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import  "./styles.css";
+import { URL } from "../../constants";
 
 const Signup = () => {
 	const [data, setData] = useState({
